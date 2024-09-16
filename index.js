8c09bd89b41fb583baf59a6d0c0651ade1bcb41d
NS-cyber <1shahnee2@hdsb.ca>
2024-09-16 12:31:19 -0400

    calculator

---
+++ index.js
@@ -0,0 +1,35 @@
+// NAME: Neev Shah
+// Date: september 16th, 2024
+// Title: Add Two Words Together 
+
+let x = prompt("What is x?");
+let y = prompt("What is y?");
+
+x = parseFloat(x);
+y = parseFloat(y);
+
+sum = x + y;
+difference = x - y;
+product = x * y;
+quotient = x / y;
+
+function addNumbers(num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+function subtractNumbers() {
+    const result = num1 - num2;
+    return result;
+}
+
+function divideNumbers() {
+    const result = num1 / num2;
+    return result;
+}
+
+function multiplyNumbers() {
+    const result = num1 * num2
+    return result;
+}
+
