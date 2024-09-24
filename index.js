690eb84f93747feb4fa1b07fc06f79087cb8fe59
NS-cyber <1shahnee2@hdsb.ca>
2024-09-24 12:07:55 -0400

    calculator assignment, added and edited flaws

--- index.js
+++ index.js
@@ -5,29 +5,41 @@
 
 
 
-operation = prompt("What operation do you want to use? (+, -, /, *, pow, or Sqrt)")
+// collect user information for calculation 
 
-if (operation = "sqrt") {
-    prompt("What is x?");
+
+operation = prompt("What operation do you want to use? (+, -, /, *, pow, or sqrt)")
+
+let x= 0;
+let y= 0;
+
+if (operation == "sqrt") {
+    x = prompt("What is the number you wish to find the sqrt of?");
 } else if (operation != "sqrt") {
-    prompt("What is x?"); 
-    prompt("What is y");
+    x = prompt("What is the first number?"); 
+    y = prompt("What is the second number?");
 }
-let x = prompt("What is x?");
-let y = prompt("What is y?");
+// this converts the x and y values from a string to an intger 
 
 x = parseFloat(x);
 y = parseFloat(y);
 
 let result;
 
+//if x and y are not integers the users will be promted with an error
+if (!Number.isInteger(operation)) {
+    alert('please enter a valid operation to continue')
+}
+
 if (!Number.isInteger(x)) {
-    alert("Please enter a valid integer for x");
+    alert("Please enter a valid integer for your first number");
 }   
 
 if (!Number.isInteger(y)) {
-    alert("Please enter a valid integer for y");
+    alert("Please enter a valid integer for your second number");
 }   
+
+// Giving the results to the users depending on the operation they entered
 
 if (operation == "+") {
     result = x + y;
@@ -40,15 +52,8 @@ if (operation == "+") {
 } else if (operation == "pow") {
    result = Math.pow(x, y);
 } else if (operation == "sqrt") {
-    result = Math. sqrt(x)
+    result = Math.sqrt(x)
 }
 else alert("")
 
 console.log(result);
-
-
-
-/* Setting a number for x and a letter for y results in NaN but is different vice versa. 
-
-If the program reads the first number as an integer, the proper error message appears, but
-if x is an integer while y is not, NaN appears*/
